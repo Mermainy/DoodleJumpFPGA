d0283b880613ae99ba3c5c87818c3dc8d7b1251c
Danil <bintifield@gmail.com>
2025-05-18 10:32:21 +0300

    Initial commit

---
+++ Design/platforms.sv
@@ -0,0 +1,7 @@
+module platforms(
+	output [4:0][3:0][:] platforms
+)
+
+
+
+endmodule
---
+++ board_specific.sv
@@ -0,0 +1,9 @@
+module board_specific(
+	input MAX10_CLK1_50,
+	
+	output clk
+)
+
+assign clk = MAX10_CLK1_50;
+
+endmodule
