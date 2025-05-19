54c32d00e7d463a9d1a433729e046ebd5163bb5c
Mike Kuskov <m.kuskov@innopolis.university>
2025-05-20 00:53:34 +0300

    feat: sprite generation script

--- Scripts/generate_sprites.py
+++ Scripts/generate_sprites.py
@@ -1,32 +1,90 @@
 import os
+from pathlib import Path
 
 from PIL import Image
 
 
-image_path = "Platform100x30.png"
-entity_name = "green_platform"
+PROJECT_DIR = Path('./')  # .resolve()
+SPRITES_DIR = PROJECT_DIR / 'Sprites'
+TEXTURES_DIR = PROJECT_DIR / 'Design' / 'textures'
 
-image = Image.open(image_path)
-pixels = image.load()
-
-try:
-    os.unlink("output_color.sv")
-    os.unlink("output_transparency.sv")
-except FileNotFoundError:
-    pass
-
-output_file_color = open("output_color.sv", 'a', encoding="utf-8")
-output_file_transparency = open("output_transparency.sv", 'a', encoding="utf-8")
-
-for y in range(image.height):
-    for x in range(image.width):
-        output_file_color.write(f"\t{entity_name}_texture[{y}][{x}] = "
-                          f"{{4'b{bin(pixels[x, y][2] // 17)[2:]}, "
-                                f"4'b{bin(pixels[x, y][1] // 17)[2:]}, "
-                                f"4'b{bin(pixels[x, y][0] // 17)[2:]}}};")
-for y in range(image.height):
-    for x in range(image.width):
-        output_file_transparency.write(f"\t{entity_name}_transparency_texture[{y}][{x}] "
-                                       f"= {int(not bool(pixels[x, y][3]))};")
+QUARTUS_PROJECT_FILE = next(PROJECT_DIR.glob('*.qpf'))
+QUARTUS_SETTINGS_FILE = next(PROJECT_DIR.glob('*.qsf'))
 
 
+assert QUARTUS_PROJECT_FILE.exists(), f'Quartus project file not found "{QUARTUS_PROJECT_FILE}"'
+assert QUARTUS_SETTINGS_FILE.exists(), f'Quartus settings file not found "{QUARTUS_SETTINGS_FILE}"'
+TEXTURES_DIR.mkdir(parents=True, exist_ok=True)
+
+
+def main():
+    for image_path in SPRITES_DIR.glob('*.png'):
+        image_name_raw = image_path.stem
+        image_name_components = image_name_raw.split(' ')
+
+        # if len(image_name_components) != 4:
+        #     log.warning(f'Wrong image naming convention detected "{image_path}". Skipping...')
+        #     continue
+
+        entity_name = f'{image_name_components[0]}_{image_name_components[-1]}'
+        entity_name_rgb = f'{entity_name}_rgb'
+        entity_name_alpha = f'{entity_name}_alpha'
+        macro_name = f'INITIAL_{entity_name.upper()}'
+        image_expected_width, image_expected_height = map(
+            int, image_name_components[1].split('x'))
+
+        image = Image.open(image_path)
+        pixels = image.load()
+        assert image_expected_height == image.height
+        assert image_expected_width == image.width
+
+        target_path = TEXTURES_DIR / f'{entity_name}.sv'
+
+        template_banner = (
+            f'// Module definition:\n'
+            f'// logic [{image.height-1}:0][{image.width-1}:0][2:0][3:0] {entity_name_rgb};\n'
+            f'// logic [{image.height-1}:0][{image.width-1}:0][2:0][3:0] {entity_name_alpha};'
+        )
+        template_header = (
+            f'`ifndef {macro_name}\n\n{template_banner}\n\n'
+            f'`define {macro_name} \\\nalways_comb begin \\'
+        )
+        template_footer = f'end\n\n`endif // {macro_name}\n'
+
+        # Construct template
+        file_lines = []
+        file_lines.append(template_header)
+        for y in range(image.height):
+            for x in range(image.width):
+                file_lines.append(
+                    f"\t{entity_name_rgb}[{y}][{x}] = "
+                    f"{{4'b{bin(pixels[x, y][2] // 17)[2:]}, "
+                    f"4'b{bin(pixels[x, y][1] // 17)[2:]}, "
+                    f"4'b{bin(pixels[x, y][0] // 17)[2:]}}}; \\"
+                )
+        
+        for y in range(image.height):
+            for x in range(image.width):
+                file_lines.append(
+                    f"\t{entity_name_alpha}[{y}][{x}] "
+                    f"= 1'b{int(not bool(pixels[x, y][3]))}; \\"
+                )
+        file_lines.append(template_footer)
+
+        # Render filled template to the SystemVerilog file
+        target_path.write_text('\n'.join(file_lines))
+
+        update_quartus_settings_file(target_path)
+
+
+def update_quartus_settings_file(generated_filepath: Path):
+    qsf_content = QUARTUS_SETTINGS_FILE.read_text()
+
+    if generated_filepath.name not in qsf_content:
+        qsf_content += f'\nset_global_assignment -name SYSTEMVERILOG_FILE {generated_filepath.as_posix()}\n'
+        QUARTUS_SETTINGS_FILE.write_text(qsf_content)
+        print(f'File {generated_filepath} appended to the Quartus Settings file')
+
+
+if __name__ == '__main__':
+    main()
